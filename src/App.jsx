fd133419ebf04dc6032a034243d6d13283e8a633
Andrey Navoitsev <evil.zlo2018@mail.ru>
2025-02-04 21:08:03 +0300

    fix: background animation
    I fixed the animation a bit because it was very small, I didn't like it, so I changed it and positioned it in the center, now the background will look good on all devices.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
-import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 function App() {
   return (
     <>
-    <DotLottieReact className="centralitys"
-      src="https://lottie.host/af563d04-5a31-4223-be44-148aacf29543/sCI2lijgje.lottie"
-      loop
-      autoplay
-    />
+      <div className="centralitys">
+        <DotLottieReact
+          src="https://lottie.host/af563d04-5a31-4223-be44-148aacf29543/sCI2lijgje.lottie"
+          loop
+          style={{width: '400px', height: '400px'}}
+          autoplay
+        />
+      </div>
+
       <Header />
       <Main />
       <Footer />
