282b712539cffc22ece5fa5dcd1ed26d0d2be60c
Andrey Navoitsev <evil.zlo2018@mail.ru>
2025-02-05 22:27:42 +0300

    Improved: styles of the task control block
    At the moment, I have used the timer library and connected the material-icon library. In the future, I will manage it, stylize the buttons and prepare everything for further work.

--- src/components/Main/StatusBar/StatusBar.jsx
+++ src/components/Main/StatusBar/StatusBar.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./StatusBar.module.css";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 export const StatusBar = () => {
+  const [play, setPlay] = useState(true);
+
+  const togglePlay = (e) => {
+    e.preventDefault();
+    setPlay((state) => {
+      return (state = !state);
+    });
+  };
+
   return (
     <div className={style["status__bar"]}>
       <h2 className={style["header"]}>Название задачи!</h2>
-      <span> круговой таймер</span>
-      <div className="controllers ">
-        <i>
-          <a href="#">галочка</a>
-        </i>
-        <i>
-          <a href="#">Пауза/плей</a>
-        </i>
-        <i>
-          <a href="#">крестик</a>
-        </i>
+      <div className={style["timer"]}>
+        {" "}
+        <CountdownCircleTimer
+          isPlaying
+          duration={7}
+          colors={["#004777"]}
+          // colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          // colorsTime={[7, 5, 2, 0]}
+        >
+          {({ remainingTime }) => remainingTime}
+        </CountdownCircleTimer>
+      </div>
+
+      <div className={style["controllers"]}>
+        <a href="#">
+          <span className={`material-icons ${style['green']}` } >done_all</span>
+        </a>
+        <a href="#" onClick={(e) => togglePlay(e)}>
+          {play ? (
+            <span className={`material-icons ${style['yellow']}` } >pause</span>
+          ) : (
+            <span className={`material-icons ${style['yellow']}` }>play_arrow</span>
+          )}
+        </a>
+        <a href="#">
+          <span className={`material-icons ${style['red']}` }>close</span>
+        </a>
       </div>
     </div>
   );
