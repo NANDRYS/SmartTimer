a2e2ed58560f06909c1877bbb119cf1094e50ede
Andrey Navoitsev <evil.zlo2018@mail.ru>
2025-02-11 20:40:43 +0300

    deploy

--- src/components/Main/Button/Button.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import style from "../../../assets/css/MainStyle/Button.module.css";
-
-export const Button = () => {
-  return <button className={style['button']}>Created</button>;
-};
